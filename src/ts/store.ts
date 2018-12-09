2fbc2d29bbec498e6db6eb562dd747a77949ed7e
UzakiToba <yukiuzu0912@gmail.com>
2018-12-09 14:48:09 +0900

    devtool実装

--- src/ts/store.ts
+++ src/ts/store.ts
@@ -1,5 +1,14 @@
 import { createStore } from 'redux';
+// devTool
+import { composeWithDevTools } from 'redux-devtools-extension';
+const composeEnhancers = composeWithDevTools({});
 
 import reducer from './redux/';
 
-export const store = createStore(reducer);
+export const store = createStore(
+  reducer,
+  (() => {
+    // 開発時だけdevtoolを使用できるようにする
+    if (process.env.NODE_ENV === 'development') return composeEnhancers();
+  })()
+);
